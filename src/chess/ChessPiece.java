10aa67931b94126bcae7437a006f79e438659a43
juniorchavesv <chavesjuniorvasconcelos@gmail.com>
2025-08-18 13:51:08 -0300

    Possible moves of a piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,7 +4,7 @@ import boardgame.Board;
 import boardgame.Piece;
 
 // PEÇA DE XADREZ
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 	
 	private Color color;
 
